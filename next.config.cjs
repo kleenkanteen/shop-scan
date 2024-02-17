4559c5d6bdac1b0f5a1395c83f654e89cb9960e3
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-02-17 22:37:58 +0200

    feat: add pwa to next config and create manifest.json

--- next.config.cjs
+++ next.config.cjs
@@ -3,8 +3,11 @@
  * for Docker builds.
  */
 await import("./src/env.js");
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
 
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
+module.exports = withPWA(config);
