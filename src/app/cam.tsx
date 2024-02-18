158be916a3fc5580c8cc2c75a1ba8072b61e0370
Slavi Ivanov Karaslavov <106228555+Slaviiiii@users.noreply.github.com>
2024-02-18 19:41:46 +0200

    feat: add photo capturing (#4)

---
+++ src/app/cam.tsx
@@ -0,0 +1,51 @@
+"use client";
+import Image from "next/image";
+import { useRef, useState, useEffect } from "react";
+
+export default function Cam() {
+  const [image, setImage] = useState<string | null>(null);
+  const videoPlayerRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const initializeMedia = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        if (videoPlayerRef.current) {
+          videoPlayerRef.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.log("Error accessing camera:", error);
+      }
+    };
+
+    initializeMedia();
+  }, []);
+
+  const handleCapture = () => {
+    const video = videoPlayerRef.current;
+    const canvas = canvasRef.current;
+    if (video && canvas) {
+      const context = canvas.getContext("2d");
+      if (context) {
+        context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+        canvas.toBlob((blob) => {
+          if (blob) {
+            setImage(URL.createObjectURL(blob));
+          }
+        }, "image/jpeg");
+      }
+    }
+  };
+
+  return (
+    <>
+      <video ref={videoPlayerRef} id="player" autoPlay></video>
+      <canvas ref={canvasRef} style={{ display: "none" }}></canvas>
+      <button onClick={handleCapture}>Click here to capture</button>
+      {image && <Image src={image} width={500} height={500} alt="Captured" />}
+    </>
+  );
+}
