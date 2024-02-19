487a76d13b9d23bc8693da2edd0695c238338c53
Matthew Hutter <matthewjhutter@gmail.com>
2024-02-18 17:25:08 -0800

    BG and app title rough styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Cam from "./cam";
 
 export default function HomePage() {
   return (
-    <main>
-      home page
+    <main className="bg-purple-300">
+      <h1 className="font-bold text-2xl">
+        Shop Scan
+      </h1>
       <Cam />
     </main>
   );
