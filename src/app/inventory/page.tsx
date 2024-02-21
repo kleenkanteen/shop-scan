95b334b17dcd8f3e8b8e0f8ca2acafa2f4486e77
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-02-21 23:29:01 +0200

    feat: use shadcn components in inventory

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,12 +1,41 @@
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { supabase } from "../utils/supabase/client";
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 export default async function Inventory() {
-  const { data: inventory } = await supabase.from("inventory").select();
-
+  // const { data: inventory } = await supabase.from("inventory").select();
+  const inventory: any = [];
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
-      Inventory
-      {inventory?.map((inventory) => <div>{inventory.name}</div>)}
+    <div className="flex h-full flex-col items-center justify-evenly">
+      <h1 className="text-5xl font-medium">Inventory</h1>
+
+      {inventory.length > 0 ? (
+        inventory?.map((inventory: any) => (
+          <Link href={""}>
+            <Card className="w-[350px] hover:bg-slate-50">
+              <CardHeader>
+                <CardTitle className="text-2xl font-bold">
+                  {inventory.name}
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex justify-between text-xl">
+                <div className="font-bold text-gray-500">
+                  {inventory.price}$
+                </div>
+                <div className="font-bold text-gray-500">
+                  In stock: {inventory.stock}
+                </div>
+              </CardContent>
+            </Card>
+          </Link>
+        ))
+      ) : (
+        <div className="flex flex-col gap-4">
+          <p className="text-xl font-normal">Your inventory is empty</p>
+          <Button>Add first product</Button>
+        </div>
+      )}
     </div>
   );
 }
