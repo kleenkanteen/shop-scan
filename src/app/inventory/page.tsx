afd97722a1c58a97cecea854cde49587512d1d6a
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-02-22 20:38:45 +0200

    feat: create add products page

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,7 +1,6 @@
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
-import { supabase } from "../utils/supabase/client";
 import Link from "next/link";
-import { Button } from "~/components/ui/button";
+import { buttonVariants } from "~/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 
 export default async function Inventory() {
   // const { data: inventory } = await supabase.from("inventory").select();
@@ -12,28 +11,26 @@ export default async function Inventory() {
 
       {inventory.length > 0 ? (
         inventory?.map((inventory: any) => (
-          <Link href={""}>
-            <Card className="w-[350px] hover:bg-slate-50">
-              <CardHeader>
-                <CardTitle className="text-2xl font-bold">
-                  {inventory.name}
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="flex justify-between text-xl">
-                <div className="font-bold text-gray-500">
-                  {inventory.price}$
-                </div>
-                <div className="font-bold text-gray-500">
-                  In stock: {inventory.stock}
-                </div>
-              </CardContent>
-            </Card>
-          </Link>
+          <Card className="w-[350px] hover:bg-slate-50">
+            <CardHeader>
+              <CardTitle className="text-2xl font-bold">
+                {inventory.name}
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex justify-between text-xl">
+              <div className="font-bold text-gray-500">{inventory.price}$</div>
+              <div className="font-bold text-gray-500">
+                In stock: {inventory.stock}
+              </div>
+            </CardContent>
+          </Card>
         ))
       ) : (
         <div className="flex flex-col gap-4">
           <p className="text-xl font-normal">Your inventory is empty</p>
-          <Button>Add first product</Button>
+          <Link href={"/inventory/add-product"} className={buttonVariants()}>
+            Add first product
+          </Link>
         </div>
       )}
     </div>
